8a1c928eb9098b04976a188912caae84d4bb881f
Lochlan Wansbrough <lochlan.wansbrough@workatplay.com>
2016-08-15 09:29:44 -0700

    First commit

---
+++ HolographicJS/AppViewSource.cs
@@ -0,0 +1,13 @@
+﻿using Windows.ApplicationModel.Core;
+
+namespace HolographicJS
+{
+    // The entry point for the app.
+    internal class AppViewSource : IFrameworkViewSource
+    {
+        public IFrameworkView CreateView()
+        {
+            return new AppView();
+        }
+    }
+}
---
+++ HolographicJS/Assets/js/app.js
@@ -0,0 +1,12 @@
+﻿let canvas = document.createElement('canvas');
+console.log(canvas);
+console.log('Canvas created');
+try {
+    let context = canvas.getContext('webgl-holographic');
+    console.log('Got context:');
+    console.log(context);
+}
+catch (e) {
+    console.log(e.message);
+    console.log(e.description);
+}
